080bb74e320fccba846f88cff688195666e8a18b
Douglas Casserleigh <casserleigh@gmail.com>
2016-02-26 15:24:28 -0700

    Created necessary files to facilitate completion of lrthw_chapter_3_homework
    [#113796217]

---
+++ chapter_3/ex3.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+# I will count chickens
+puts "I will now count my chickens:"
+# equation for # of hens
+puts "Hens #{25 + 30 / 6}"
+# equation for # of roosters
+puts "Roosters #{100 - 25 * 3 % 4}"
+# I will count chickens
+puts "Now I will count the eggs:"
+# equation for # of eggs
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# boolean math proposition
+puts "Is it true that 3 + 2 < 5 - 7?"
+# boolean result = false
+puts 3 + 2 < 5 - 7
+# boolean result analysis
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+# thats why false
+puts "Oh, that's why it's false."
+#  lets do more
+puts "How about some more."
+# more boolean comparisons
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
---
+++ chapter_3/number_of _seconds_in_davinci_class.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+number_seconds_minute = 60
+number_minutes_hour = 60
+number_hours_class = 3
+number_classes_month = 12
+number_months_program = 3
+
+puts "The number of seconds spent in Davinci class is #{number_seconds_minute * number_minutes_hour * number_hours_class * number_classes_month * number_months_program} seconds!"
