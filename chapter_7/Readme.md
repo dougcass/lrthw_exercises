6cfa3c6049b401e1b15b1202449c8ab8b9dba834
Douglas Casserleigh <casserleigh@gmail.com>
2016-03-04 13:42:34 -0700

    Created necessary files to complete lrthw chapter 7 homework
    [#113796225]

---
+++ chapter_7/Readme.md
@@ -0,0 +1,7 @@
+##The difference between print and puts
+
+Print outputs a designated value and remains on the same line
+in the terminal
+
+Puts ouputs a designated value and returns a new line in the 
+terminal
---
+++ chapter_7/ex7.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+# outputs a string
+puts "Mary had a little lamb."
+# outputs a string
+puts "Its fleece was white as #{'snow'}."
+# outputs a string
+puts "And everywhere that Mary went."
+# outputs a string multipled by a number
+puts "." * 10 # what'd that do?
+# defines a variable for a string
+end1 = "C"
+# defines a variable for a string
+end2 = "h"
+# defines a variable for a string
+end3 = "e"
+# defines a variable for a string
+end4 = "e"
+# defines a variable for a string
+end5 = "s"
+# defines a variable for a string
+end6 = "e"
+# defines a variable for a string
+end7 = "B"
+# defines a variable for a string
+end8 = "u"
+# defines a variable for a string
+end9 = "r"
+# defines a variable for a string
+end10 = "g"
+# defines a variable for a string
+end11 = "e"
+# defines a variable for a string
+end12 = "r"
+
+# watch that print vs. puts on this line what's it do?
+# outputs concatenated strings as defined by the variables
+# and remains on same line
+print end1 + end2 + end3 + end4 + end5 + end6
+# outputs concatenated strings as defined by the variables
+# and returns a new line
+puts end7 + end8 + end9 + end10 + end11 + end12
+
