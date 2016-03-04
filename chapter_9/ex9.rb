e272a78f2b6266dde05775e09e3aa9f9646a7770
Douglas Casserleigh <casserleigh@gmail.com>
2016-03-04 15:34:17 -0700

    Created necessary files to complete lrthw chapter 9 homework
    [#113796229]

---
+++ chapter_9/ex9.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+# Here's some strange new stuff, remember type it exactly
+
+days = 'Mon Tue Wed Thu Fri Sat Sun'
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\n"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts "There's something going on here\nWith the three double-quotes."
+puts "We'll be able to type as much as we like."
+puts 'Even 4 lines if we want, or 5, or 6.'
