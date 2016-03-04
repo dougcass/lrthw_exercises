e56dbe86883bea7626786db3b202fe749a027d5c
Douglas Casserleigh <casserleigh@gmail.com>
2016-03-04 12:32:22 -0700

    Fixed Rubocop offenses
    [#113796217]

--- chapter_3/number_of _seconds_in_davinci_class.rb
+++ chapter_3/number_of _seconds_in_davinci_class.rb
@@ -5,5 +5,8 @@ number_minutes_hour = 60
 number_hours_class = 3
 number_classes_month = 12
 number_months_program = 3
+number_seconds_class = number_seconds_minute * number_minutes_hour * number_hours_class
 
-puts "The number of seconds spent in Davinci class is #{number_seconds_minute * number_minutes_hour * number_hours_class * number_classes_month * number_months_program} seconds!"
+puts 'The number of seconds spent in Davinci class is ' \
+  "#{number_seconds_class * number_classes_month * number_months_program}"
++'seconds!'
