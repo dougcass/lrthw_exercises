4b47dc74163ee927295d457818cc7d2dcfd59ea7
Douglas Casserleigh <casserleigh@gmail.com>
2016-03-04 20:20:52 -0700

    Edited code in reponse to Rubocop offenses.
    [#113796225]

--- chapter_7/Readme.md
+++ chapter_7/Readme.md
@@ -3,5 +3,11 @@
 Print outputs a designated value and remains on the same line
 in the terminal
 
-Puts ouputs a designated value and returns a new line in the 
+Puts outputs a designated value and returns a new line in the 
 terminal
+
+##Rubocop offenses:
+
+offense: Prefer single quotes.
+solution: Replaced double quotes with single quotes where possible.
+
--- chapter_7/ex7.rb
+++ chapter_7/ex7.rb
@@ -1,36 +1,36 @@
 #!/usr/bin/env ruby
 # outputs a string
-puts "Mary had a little lamb."
+puts 'Mary had a little lamb.'
 # outputs a string
 puts "Its fleece was white as #{'snow'}."
 # outputs a string
-puts "And everywhere that Mary went."
+puts 'And everywhere that Mary went.'
 # outputs a string multipled by a number
-puts "." * 10 # what'd that do?
+puts '.' * 10 # what'd that do?
 # defines a variable for a string
-end1 = "C"
+end1 = 'C'
 # defines a variable for a string
-end2 = "h"
+end2 = 'h'
 # defines a variable for a string
-end3 = "e"
+end3 = 'e'
 # defines a variable for a string
-end4 = "e"
+end4 = 'e'
 # defines a variable for a string
-end5 = "s"
+end5 = 's'
 # defines a variable for a string
-end6 = "e"
+end6 = 'e'
 # defines a variable for a string
-end7 = "B"
+end7 = 'B'
 # defines a variable for a string
-end8 = "u"
+end8 = 'u'
 # defines a variable for a string
-end9 = "r"
+end9 = 'r'
 # defines a variable for a string
-end10 = "g"
+end10 = 'g'
 # defines a variable for a string
-end11 = "e"
+end11 = 'e'
 # defines a variable for a string
-end12 = "r"
+end12 = 'r'
 
 # watch that print vs. puts on this line what's it do?
 # outputs concatenated strings as defined by the variables
@@ -39,4 +39,3 @@ print end1 + end2 + end3 + end4 + end5 + end6
 # outputs concatenated strings as defined by the variables
 # and returns a new line
 puts end7 + end8 + end9 + end10 + end11 + end12
-
