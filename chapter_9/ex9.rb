f4b3082be8c709c4c154c9473ef9121734638243
Douglas Casserleigh <casserleigh@gmail.com>
2016-03-23 20:23:34 -0500

    Bundled code output using multiple line puts format
    [#113796229]

--- chapter_9/ex9.rb
+++ chapter_9/ex9.rb
@@ -5,9 +5,10 @@
 days = 'Mon Tue Wed Thu Fri Sat Sun'
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\n"
 
-puts "Here are the days: #{days}"
-puts "Here are the months: #{months}"
-
-puts "There's something going on here\nWith the three double-quotes."
-puts "We'll be able to type as much as we like."
-puts 'Even 4 lines if we want, or 5, or 6.'
+puts """
+Here are the days: #{days}
+Here are the months: #{months}
+There's something going on here\nWith the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
