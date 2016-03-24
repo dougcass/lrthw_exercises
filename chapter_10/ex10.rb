798fe52b4374dc6ce9d73dcbf76a6c6409e5589f
Douglas Casserleigh <casserleigh@gmail.com>
2016-03-23 21:32:42 -0500

    Edited code format to fix rubocop errors
    [#113796231]

--- chapter_10/ex10.rb
+++ chapter_10/ex10.rb
@@ -4,13 +4,8 @@ tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
 
-fat_cat = """
-I'll do a list:
-\t* Cat food
-\t* Fishies
-\t* Catnip\n\t* Grass
-"""
-
+fat_cat = "I'll do a list: \n\t * Cat food \n\t * Fishies \n\t" \
+          " * Catnip \n\t * Grass"
 puts tabby_cat
 puts persian_cat
 puts backslash_cat
