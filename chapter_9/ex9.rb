ec733cd432dd91c4d0b99c9d3a51238137e44989
Douglas Casserleigh <casserleigh@gmail.com>
2016-04-04 18:22:53 -0600

    Fixed rubocop string error for multiple line formatting
    [#113796229]

--- chapter_9/ex9.rb
+++ chapter_9/ex9.rb
@@ -5,10 +5,10 @@
 days = 'Mon Tue Wed Thu Fri Sat Sun'
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\n"
 
-puts """
+puts "
 Here are the days: #{days}
 Here are the months: #{months}
 There's something going on here\nWith the three double-quotes.
 We'll be able to type as much as we like.
 Even 4 lines if we want, or 5, or 6.
-"""
+"
